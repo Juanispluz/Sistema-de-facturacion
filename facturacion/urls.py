c406fab92c83602ac2d2d8653517904b6bce7d34
Karlos Herrera <juancarlosramirezherrera985@hotmail.com>
2024-10-08 10:11:55 -0500

    XD

--- facturacion/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-# Urls de la aplicación facturación
-
-urlpatterns = [
-    path("", views.index, name="index"),
-]
--- readme.md
+++
@@ -1,7 +0,0 @@
-Python 3.12.7
-Django 5.1.1
-
-Super usuario
-User: root
-Email: root@root.com
-Password: root
